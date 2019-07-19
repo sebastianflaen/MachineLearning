2c0c06272db3a4cbbc88cabac368f3a29269caf7
SebastiansMac <sebastianflaen@gmail.com>
2019-07-19 19:14:34 +0200

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Sebastian Sundet Flaen on 19/07/2019.
+//  Copyright © 2019 ssflaen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
