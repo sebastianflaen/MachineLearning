0ccb4478dab3636de1468c697a8871d272a28ffd
SebastiansMac <sebastianflaen@gmail.com>
2019-07-21 23:47:55 +0200

    image recognizer finished

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false //ingen cropping
+        //implements camera
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            
+            imageView.image = userPickedImage
+            
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Kunne ikke konvertere til ciiImage")
+            }
+            detect(image: ciImage)
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("loading coreML failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process image")
+            }
+            print(results)
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("Bil"){
+                    self.navigationItem.title = "Car"
+                }else {
+                    self.navigationItem.title = "Ikke en bil"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try! handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+        
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
